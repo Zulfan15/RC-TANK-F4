8effaa1200a5811585e06d5d322b36bf8889c80e
RakhaDwiPradifa <rakhadwipradifa05@gmail.com>
2024-12-14 20:04:28 +0700

    small change in sidebar

--- src/pages/landing_page/index.vue
+++ src/pages/landing_page/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="w-full flex flex-col bg-[#FCFAEE] min-h-screen overflow-hidden">
     <!-- Navbar -->
-    <div class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 shadow-sm w-full h-12 sm:h-12 md:h-12 lg:h-12 fixed top-0 z-50 flex items-center gap-4 px-4 sm:px-6 md:px-8 lg:px-10 shadow-lg">
+    <div class="bg-[#FCFAEE] backdrop-filter backdrop-blur-lg bg-opacity-30 shadow-sm w-full h-12 sm:h-12 md:h-12 lg:h-12 fixed top-0 z-50 flex items-center gap-4 px-4 sm:px-6 md:px-8 lg:px-10 shadow-lg">
       <div class="bg-red-500 sm:ml-4 md:ml-6 lg:ml-6 rounded-full w-10 h-10 sm:w-12 sm:h-10 md:w-10 md:h-10 lg:w-10 lg:h-10"></div>
       <div class="bg-yellow-500 rounded-full w-10 h-10 sm:w-12 sm:h-10 md:w-10 md:h-10 lg:w-10 lg:h-10"></div>
       <div class="bg-green-500 rounded-full w-10 h-10 sm:w-12 sm:h-10 md:w-10 md:h-10 lg:w-10 lg:h-10"></div>
@@ -9,10 +9,10 @@
 
     <div class="flex flex-grow flex-col md:flex-row mt-20">
       <!-- Sidebar -->
-      <aside class="w-14 sm:w-14 md:w-14 lg:w-14 h-screen fixed top-11 left-0 px-4 bg-gradient-to-b from-[#91091E] to-[#1E1E1E] flex flex-col items-center py-2 space-y-6 shadow-lg flex-shrink-0">
+      <aside class="w-14 sm:w-14 md:w-14 lg:w-14 h-screen fixed top-11 left-0 px-4 bg-[#91091E] flex flex-col items-center py-2 space-y-6 shadow-lg flex-shrink-0">
         <!-- Logo -->
         <div class="mb-2 mt-4 transition-transform hover:scale-110 duration-300">
-          <div class="w-12 sm:w-12 md:w-12 lg:w-12 sm:h-12 md:h-12 lg:h-12 bg-[#FCFAEE] rounded-xl flex items-center justify-center shadow-lg transform hover:scale-110 transition-transform duration-300">
+          <div class="w-12 sm:w-12 md:w-12 lg:w-12 sm:h-12 md:h-12 lg:h-12 bg-[#FFF0D1] rounded-xl flex items-center justify-center shadow-lg transform hover:scale-110 transition-transform duration-300">
             <a href="/">
               <img class="w-fit h-fit p-2 group-hover:scale-110 transition-transform duration-300"
               src="@/assets/tank.png" alt="Logo" />
@@ -22,15 +22,15 @@
         <!-- Navigation -->
         <nav class="flex flex-col gap-6">
           <div class="w-12 sm:w-12 md:w-12 lg:w-12 sm:h-12 md:h-12 lg:h-12">
-            <a href="/data" class="flex items-center justify-center rounded-xl bg-[#FCFAEE] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
+            <a href="/data" class="flex items-center justify-center rounded-xl bg-[#FFF0D1] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
             <img class="w-12 h-12 md:w-14 md:h-12 p-2 group-hover:scale-110 transition-transform duration-300" src="@/assets/angkadata.png" alt="Data" />
           </a>
           </div>
 
-          <a href="/charts" class="flex items-center justify-center rounded-xl bg-[#FCFAEE] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
+          <a href="/charts" class="flex items-center justify-center rounded-xl bg-[#FFF0D1] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
             <img class="w-12 h-12 md:w-12 md:h-12 p-2 group-hover:scale-110 transition-transform duration-300" src="@/assets/charts.png" alt="Charts" />
           </a>
-          <a href="/history" class="flex items-center justify-center rounded-xl bg-[#FCFAEE] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
+          <a href="/history" class="flex items-center justify-center rounded-xl bg-[#FFF0D1] shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-400">
             <img class="w-12 h-12 sm:w-12 sm:h-12 md:w-12 md:h-12 p-2 group-hover:scale-110 transition-transform duration-300" src="@/assets/history.png" alt="History" />
           </a>
         </nav>
